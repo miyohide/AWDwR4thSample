cf81065168cfd3894b484001c87cbf7ea565435b
miyohide <miyohide@gmail.com>
2012-02-19 16:10:57 +0900

    i18n prepare done.

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 class ApplicationController < ActionController::Base
+   before_filter :set_i18n_locale_from_params
    before_filter :authorize
 
    protect_from_forgery
@@ -20,5 +21,22 @@ class ApplicationController < ActionController::Base
          redirect_to login_url, notice: "ログインしてください"
       end
    end
+
+   def set_i18n_locale_from_params
+      if params[:locale]
+         if I18n.available_locales.include?(params[:locale].to_sym)
+            I18n.locale = params[:locale]
+         else
+            flash.now[:notice] =
+               "#{params[:locale]} translation not available"
+            logger.error flash.now[:notice]
+         end
+      end
+   end
+
+   def default_url_options
+      { locale: I18n.locale }
+   end
+
 
 end
