6b13e1de9f682c9f446c0656eeca5d28257a073a
miyohide <miyohide@gmail.com>
2012-02-19 14:50:30 +0900

    common controller method authorize implement

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+   before_filter :authorize
 
-  private
-  def current_cart
-     Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-     cart = Cart.create
-     session[:cart_id] = cart.id
-     cart
-  end
+   protect_from_forgery
+
+   private
+   def current_cart
+      Cart.find(session[:cart_id])
+   rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+   end
+
+   private
+   def authorize
+      unless User.find_by_id(session[:user_id])
+         redirect_to login_url, notice: "ログインしてください"
+      end
+   end
 
 end
